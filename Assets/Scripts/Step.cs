8779113933f11e4f2a1a0700706fe40e563f57a3
nakashu195 <tk220195@tks.iput.ac.jp>
2024-12-20 12:45:36 +0900

    踏み台の実装 part2

---
+++ Assets/Scripts/Step.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step : MonoBehaviour
+{
+    public GameObject DestObj; // Inspector で設定可能に
+    public GameObject JeneObj; // 生成するプレハブ
+    public int stepnumber = 0;
+
+    // GameObjectの生成と破壊
+    public void CreateAndDestroy()
+    {
+        if (JeneObj != null)
+        {
+            // Y軸回転を保持する
+            Quaternion rotation = Quaternion.identity; // デフォルト回転
+            if (DestObj != null)
+            {
+                // DestObjのY軸回転を取得
+                rotation = Quaternion.Euler(0, DestObj.transform.eulerAngles.y, 0);
+            }
+
+            // JeneObjを生成
+            Instantiate(JeneObj, transform.position, rotation);
+            Debug.Log("オブジェクトを生成しました");
+        }
+        else
+        {
+            Debug.LogError("JeneObj が設定されていません！");
+        }
+
+        if (DestObj != null)
+        {
+            // DestObj を破壊
+            Destroy(DestObj);
+            DestObj = null; // 参照をクリア
+            Debug.Log("オブジェクトを破壊しました");
+        }
+        else
+        {
+            Debug.LogWarning("DestObj が設定されていません。");
+        }
+    }
+}
---
+++ Assets/Scripts/StepUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepUp : MonoBehaviour
+{
+    public GameObject stepObj; // 移動先のオブジェクト
+    public GameObject Player; // インスペクターで設定されたプレイヤーオブジェクト（nullでも構わない）
+
+    public void stepup()
+    {
+        // シーン内の全てのGameObjectを対象にPlayerタグのオブジェクトを探索
+        GameObject playerInScene = GameObject.FindGameObjectWithTag("Player");
+
+        if (stepObj == null || playerInScene == null)
+        {
+            Debug.LogError("stepObj または シーン内のPlayer が設定されていません！");
+            return;
+        }
+
+        // stepObj の上面の位置を取得
+        Vector3 stepTopPosition = stepObj.transform.position + new Vector3(0, stepObj.transform.localScale.y / 2, 0);
+
+        // Player の大きさを考慮して少し浮かせる
+        Vector3 playerAdjustedPosition = stepTopPosition + new Vector3(0, playerInScene.transform.localScale.y / 2 - 1.0f, 0);
+
+        // Player の位置を移動
+        playerInScene.transform.position = playerAdjustedPosition;
+
+        Debug.Log("フッ!!!");
+    }
+}
